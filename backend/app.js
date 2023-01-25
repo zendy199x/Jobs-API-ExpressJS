ec7063e9fd999cba4301fa038440e31542707e8f
Zendy <nguyenduc.051194@gmail.com>
2023-01-25 15:46:38 +0700

    Define router

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const app = express();
+
+// connectDB
 const connectDB = require("./db/connect");
 const mongoose = require("mongoose");
 
-const app = express();
+mongoose.set("strictQuery", false);
+
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -14,16 +21,13 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("api/v1/auth", authRouter);
+app.use("api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
-
-mongoose.set("strictQuery", false);
 
 const start = async () => {
   try {
