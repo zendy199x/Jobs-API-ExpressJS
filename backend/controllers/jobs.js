f89604698be1d5dfc863ebb947fc6a815bf1f601
Zendy <nguyenduc.051194@gmail.com>
2023-01-26 13:44:09 +0700

    Update job

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -27,7 +27,24 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("Update job");
+  const {
+    user: { userId },
+    body: { company, position },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
