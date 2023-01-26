2d68ba30a816b0310d7057ceac12a9f339999d23
Zendy <nguyenduc.051194@gmail.com>
2023-01-26 13:36:32 +0700

    Get job

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -8,7 +8,16 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("Get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
