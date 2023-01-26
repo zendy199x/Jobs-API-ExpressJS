c4e49e242ed367fba529b6fd166dd1e21aafa39a
Zendy <nguyenduc.051194@gmail.com>
2023-01-26 13:32:17 +0700

    Get all jobs

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("Get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 const getJob = async (req, res) => {
