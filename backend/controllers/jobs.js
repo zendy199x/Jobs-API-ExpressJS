e0fd39e31b59aeaaf7275fccb03496d6ba7a604e
Zendy <nguyenduc.051194@gmail.com>
2023-01-26 13:13:51 +0700

    Create job

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,3 +1,7 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("Get all jobs");
 };
@@ -7,7 +11,9 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.body);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
