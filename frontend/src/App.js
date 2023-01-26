83f6cae22a0ec900a0dd617cdc6ff701de2cef4d
Zendy <nguyenduc.051194@gmail.com>
2023-01-26 16:08:07 +0700

    React job application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home, Dashboard, Register, Edit, Error, PrivateRoute } from './pages';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, Dashboard, Register, Edit, Error, PrivateRoute } from "./pages";
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <Home />
         </Route>
-        <PrivateRoute path='/dashboard' exact>
+        <PrivateRoute path="/dashboard" exact>
           <Dashboard />
         </PrivateRoute>
-        <Route path='/register'>
+        <Route path="/register">
           <Register />
         </Route>
-        <Route path='/edit/:id'>
+        <Route path="/edit/:id">
           <Edit />
         </Route>
 
-        <Route path='*'>
+        <Route path="*">
           <Error />
         </Route>
       </Switch>
